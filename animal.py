26d4f1604174b59d406a293c43afd425ff3e3115
ArthurMarim <arthur.lindolfo01099604@sesisenaipr.org.br>
2026-02-19 09:59:31 -0300

    atividade animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+Nome = "Cachorro" # variavel de texto
+Idade = 3 # variavel int
+Peso = 7.39 # variavel float
+Sexo = "Masculino" # variavel de texto
+
+
+print (Nome)
+print (Idade)
+print (Peso)
+print (Sexo)
