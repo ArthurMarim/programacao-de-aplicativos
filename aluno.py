06655688fd11319d70bf227bd0c04d7c670f6f9d
ArthurMarim <arthur.lindolfo01099604sesisenaipr.org.br>
2026-02-20 09:50:31 -0300

    atividade

---
+++ aluno.py
@@ -0,0 +1,10 @@
+nome = input("Digite o seu nome: ")
+telefone = int(input("Digite o seu telefone: "))
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite sua cidade: ")
+
+print ("Olá" , nome)
+print ("Seu numero é" , telefone)
+print ("Sua idade é" , idade)
+print ("voce mora em" , cidade)
+
---
+++ matematica.py
@@ -0,0 +1,13 @@
+valor_inteiro =int(input("Digite um numero inteiro: "))
+valor_inteiro2 =int(input("Digite outro valor inteiro: "))
+
+soma = valor_inteiro + valor_inteiro2
+subtracao = valor_inteiro - valor_inteiro2
+divisao = valor_inteiro / valor_inteiro2
+multiplicacao = valor_inteiro * valor_inteiro2
+
+print ("o valor da sua soma e", soma)
+print ("sua subtracao é ", subtracao)
+print ("o valor da sua divisao é", divisao)
+print ("sua multiplicacao é", multiplicacao)
+
