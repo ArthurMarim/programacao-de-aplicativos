f1c6fb011f7183c025c66019d9d5b67bb430b73f
ArthurMarim <arthur.lindolfo01099604@sesisenaipr.org.br>
2026-06-11 10:12:28 -0300

    atividade dia 11/06

--- cadastro_alunos.py
+++ cadastro_alunos.py
@@ -1,21 +1,23 @@
 import sqlite3
 conexao = sqlite3.connect('escola_demonstracao.db')
 cursor = conexao.cursor()
-cursor.execute (''' create table if not exists alunos(id_aluno integer primary key autoincrement,
-                nome text not null,
-                telefone text,
-                turma text,
-                idade integer,
-                cpf text unique not null)''')
+cursor.execute (''' CREATE TABLE IF NOT EXISTS alunos (id_aluno integer primary key autoincrement,
+                nome_aluno text NOT NULL,
+                telefone_aluno TEXT,
+                turma_aluno TEXT,
+                idade_aluno INTEGER,
+                cpf_aluno text UNIQUE NOT NULL)''')
 nome_aluno = input("Digite o nome do aluno: ") 
 telefone_aluno = input("Digite o telefone do aluno: ")
 turma_aluno = input("Digite a turma do aluno: ")
 idade_aluno = int(input("Digite a idade do aluno: "))
 cpf_aluno = input("Digite o CPF do aluno: ")
 
-comando_inserir = f'''insert into alunos (nome_aluno, telefone_aluno, turma_aluno, idade_aluno, cpf_aluno)
-                    values('{nome_aluno}','{telefone_aluno}','{turma_aluno},'{idade_aluno}'.'{idade_aluno}','{cpf_aluno}')'''
+comando_inserir = f'''INSERT INTO alunos (nome_aluno, telefone_aluno, turma_aluno, idade_aluno, cpf_aluno)
+                      VALUES('{nome_aluno}','{telefone_aluno}','{turma_aluno}',{idade_aluno},'{cpf_aluno}')'''
 
 cursor.execute(comando_inserir)
 conexao.commit()
 conexao.close()
+
+print ("Seus dados foram atualizados")
