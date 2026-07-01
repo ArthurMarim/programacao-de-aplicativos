ac253fdce9724817b907d1f208aafae224dd6148
ArthurMarim <arthur.lindolfo01099604@sesisenaipr.org.br>
2026-07-01 09:30:12 -0300

    Atividades

---
+++ trabalho/atividade13.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def verificar_registrar()
+    conexao = sqlite3.connect('sistema_escola.db')
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT * FROM alunos")
+
+    # Por que o segundo print  não mostra absolutamente nada no console?
+    print ("Primeiro print: ",cursor.fetchall())
+    print ("Segundo print: ", cursor.fetchall())
+
+    conexao.close()
+    
