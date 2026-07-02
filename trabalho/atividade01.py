1af0c4234fe22755c2e602f501f17c64a44f0201
ArthurMarim <arthurmarim01099604@sesisenaipr.org.br>
2026-07-02 09:54:55 -0300

    atividade 02/07

--- trabalho/atividade01.py
+++ trabalho/atividade01.py
@@ -14,3 +14,19 @@ def inicializar_banco():
     # O banco não está salvando as alterações. Por que?
     conexao.close()
     # O banco não está salvando as alterações pois está faltando o conexao.commit()
+
+    import sqlite3 
+
+def inicializar_banco():
+    conexao = sqlite3.connect('sistema_escola.db')
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS escolas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL  
+            )
+        ''')
+
+    conexao.commit() # o banco de dados nao foi criado
+    conexao.close() 
