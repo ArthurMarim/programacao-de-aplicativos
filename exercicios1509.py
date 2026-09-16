e1b3d4e99a05a5709b35faaf958579a5a3de2674
ArthurMarim <arthur.lindolfo1099604@sesisenaipr.org.br>
2026-09-16 09:31:16 -0300

    atividade 16/09

--- exercicios1509.py
+++ exercicios1509.py
@@ -1,4 +1,4 @@
-# def busca_sequencial(vetor, numero):
+# 1- def busca_sequencial(vetor, numero):
 #     for i in range(10):
 #         if vetor[i] == numero:
 #             return i
@@ -18,7 +18,7 @@
 
 
 
-# def contar_ocorrencias(lista, valor):
+# 2- def contar_ocorrencias(lista, valor):
 #     contador = 0
 
 #     for item in lista:
@@ -37,7 +37,7 @@
 # print("O valor aparece", quantidade, "vezes na lista.")
 
 
-# def maior_e_posicao(vetor):
+# 3- def maior_e_posicao(vetor):
 #     maior = vetor[0]
 #     posicao = 0
 
@@ -57,7 +57,7 @@
 # print("Posição:", posicao)
 
 
-# def buscar_aluno(alunos, nome):
+# 4- def buscar_aluno(alunos, nome):
 #     for aluno in alunos:
 #         if aluno == nome:
 #             return True
@@ -75,7 +75,7 @@
 
 
 
-# def buscar_posicoes(vetor, numero):
+# 5- def buscar_posicoes(vetor, numero):
 #     primeira = -1
 #     ultima = -1
 
@@ -99,5 +99,115 @@
 #     print("Última posição:", ultima)
 # else:
 #     print("Número não encontrado.")
+
+# 6- def busca_binaria(vetor, alvo):
+#     inicio = 0
+#     fim = len(vetor) - 1
+
+#     while inicio <= fim:
+#         meio = (inicio + fim) // 2
+
+#         if vetor[meio] == alvo:
+#             return meio
+#         elif vetor[meio] < alvo:
+#             inicio = meio + 1
+#         else:
+#             fim = meio - 1
+
+#     return -1
+
+# 7 - def busca_binaria_palavra(palavras, palavra):
+#     inicio = 0
+#     fim = len(palavras) - 1
+
+#     while inicio <= fim:
+#         meio = (inicio + fim) // 2
+
+#         if palavras[meio] == palavra:
+#             return meio
+#         elif palavras[meio] < palavra:
+#             inicio = meio + 1
+#         else:
+#             fim = meio - 1
+
+#     return -1
+
+
+# 8- def busca_binaria(vetor, alvo):
+#     inicio = 0
+#     fim = len(vetor) - 1
+#     comparacoes = 0
+
+#     while inicio <= fim:
+#         meio = (inicio + fim) // 2
+#         comparacoes += 1
+
+#         if vetor[meio] == alvo:
+#             return meio, comparacoes
+#         elif vetor[meio] < alvo:
+#             inicio = meio + 1
+#         else:
+#             fim = meio - 1
+
+#     return -1, comparacoes
+
+# 9- def posicao_insercao(vetor, numero):
+#     inicio = 0
+#     fim = len(vetor)
+
+#     while inicio < fim:
+#         meio = (inicio + fim) // 2
+
+#         if vetor[meio] < numero:
+#             inicio = meio + 1
+#         else:
+#             fim = meio
+
+#     return inicio
+
+
+
+# 10- def busca_sequencial(vetor, alvo):
+#     comparacoes = 0
+
+#     for i in range(len(vetor)):
+#         comparacoes += 1
+
+#         if vetor[i] == alvo:
+#             return i, comparacoes
+
+#     return -1, comparacoes
+
+
+# def busca_binaria(vetor, alvo):
+#     inicio = 0
+#     fim = len(vetor) - 1
+#     comparacoes = 0
+
+#     while inicio <= fim:
+#         meio = (inicio + fim) // 2
+#         comparacoes += 1
+
+#         if vetor[meio] == alvo:
+#             return meio, comparacoes
+#         elif vetor[meio] < alvo:
+#             inicio = meio + 1
+#         else:
+#             fim = meio - 1
+
+#     return -1, comparacoes
+
+# vetor = list(range(1, 101))
+
+# valores = [1, 50, 100]
+
+# for valor in valores:
+#     indice_seq, comp_seq = busca_sequencial(vetor, valor)
+#     indice_bin, comp_bin = busca_binaria(vetor, valor)
+
+#     print(f"Valor: {valor}")
+#     print(f"  Sequencial: índice {indice_seq}, {comp_seq} comparações")
+#     print(f"  Binária:    índice {indice_bin}, {comp_bin} comparações")
+#     print()
 
 
